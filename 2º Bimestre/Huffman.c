c79f0c00eb3d59cf1204cd550df601b25b193f56
Erick Drive <erickoliveiradesouza2021@gmail.com>
2025-06-06 01:25:01 +0000

    update

--- 2º Bimestre/Huffman.c
+++ 2º Bimestre/Huffman.c
@@ -34,7 +34,7 @@ void preencher_tab_freq(unsigned char texto[], unsigned int tab[]){
 void imprime_tam_freq(unsigned int tab[]){
 	int i;
 	
-	printf("\tTabela de Frequ�ncia\n");
+	printf("\tTabela de Frequência\n");
 	for(i = 0; i < TAM;i++){
 		if(tab[i] > 0)
 			printf("\t%d = %u = %c\n",i,tab[i],i);
@@ -95,6 +95,95 @@ void imprimir_lista(Lista *lista){
 	}
 }
 
+No* remover_inicio(Lista *lista){
+	No *aux = NULL;
+	
+	if(lista->inicio){
+		aux = lista->inicio;
+		lista->inicio = aux->proximo;
+		aux->proximo = NULL;
+		lista->tam--;
+	}
+	return aux;
+}
+
+No* montar_arvore(Lista *lista){
+	No *primeiro, *segundo, *novo;
+	while(lista->tam > 1){
+		primeiro = remover_inicio(lista);
+		segundo = remover_inicio(lista);
+		novo = malloc(sizeof(No));
+		
+		if(novo){
+			novo->caractere = '+';
+			novo->frequencia = primeiro->frequencia + segundo->frequencia;
+			novo->esq = primeiro;
+			novo->dir = segundo;
+			novo->proximo = NULL;
+			inserir_ordenado(lista, novo);
+		}else{
+			printf("\nErro ao alocar memória.\n");
+			break;
+		}
+	}
+	return lista->inicio;
+}
+
+void imprimir_arvore(No *raiz, int tam){
+	if(raiz->esq == NULL && raiz->dir == NULL)
+		printf("\tFolha: %c\tAltura: %d\n", raiz->caractere, tam);
+	else{
+		imprimir_arvore(raiz->esq, tam+1);
+		imprimir_arvore(raiz->dir, tam+1);
+	}
+}
+
+int altura(No *raiz){
+	int esq, dir;
+	
+	if(raiz == NULL)
+		return -1;
+	else{
+		esq = altura(raiz->esq) + 1;
+		dir = altura(raiz->dir) + 1;
+		
+		if(esq > dir)
+			return esq;
+		else
+			return dir;
+	}
+}
+
+char** aloca_dicionario(int colunas){
+	char **dicionario;
+	int i;
+	
+	dicionario = malloc(sizeof(char*) * TAM);
+	
+	for(i = 0; i < TAM; i++)
+		dicionario[i] = calloc(colunas, sizeof(char));
+	
+	return dicionario;		
+}
+
+void gerar_dicionario(char **dicionario, No *raiz, char *caminho, int colunas){
+	char esquerda[colunas], direita[colunas];
+	
+	if(raiz->esq == NULL && raiz->dir == NULL)
+		strcpy(dicionario[raiz->caractere], caminho);
+	else{
+		strcpy(esquerda, caminho);
+		strcpy(direita, caminho);
+		
+		strcat(esquerda, "0");
+		strcat(direita, "1");
+		
+		gerar_dicionario(dicionario, raiz->esq,  esquerda, colunas);
+		gerar_dicionario(dicionario, raiz->dir,  direita, colunas);
+	}	
+}
+
+
 int main(void){
 	unsigned char texto[] = "Vamos aprender a programar";
 	unsigned int tab_freq[TAM];
@@ -112,5 +201,9 @@ int main(void){
 	criar_lista(&lista);
 	preencher_lista(tab_freq, &lista);
 	imprimir_lista(&lista);
+	
+	arvore = montar_arvore(&lista);
+	printf("\n\tArvore de Huffman\n");
+	imprimir_arvore(arvore, 0);
 	
 }
